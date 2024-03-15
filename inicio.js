dd9bba15f6ea605ff57cfe0272c9620120fe5b17
guilherme amorim <cezarguilherme03@gmail.com>
2024-03-15 13:23:33 -0300

    arquivos adicionados

---
+++ inicio.js
@@ -0,0 +1,24 @@
+// const primeiraVariavel = "Hello World!";
+
+//(const, var, let) - Identificador - tipoDeDados
+const num = "2.0";
+const num2 = 2.0;
+const arr = ["string", 20, 2.8];
+
+num = 2;
+
+console.log(num);
+
+// console.log(arr);
+// console.log(typeof num);
+// console.log(typeof num2);
+
+// console.log(primeiraVariavel);
+
+// var primeiroNome; //int, double, arrays, objetos, null, string....
+// let segundoNome;
+
+// primeiroNome = "Guilherme"
+// segundoNome = "Amorim"
+
+// console.log(`nome completo: ${primeiroNome} ${segundoNome}`);
